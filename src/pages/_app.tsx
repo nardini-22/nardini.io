137dc7f3b8205c10c33b02ba7b8fc37f96ee0e8c
Arthur Santos Nardini <arthurnardini@ext.cvccorp.com.br>
2022-11-30 19:46:14 -0300

    initial project build

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,27 @@ import { ThemeProvider } from 'styled-components'
 
 import GlobalStyle from '../styles/global'
 import theme from '../styles/theme'
+import Head from 'next/head'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
+    <>
+    <Head>
+        <title>Nardini.io</title>
+        {/* <link rel="shortcut icon" href="/img/icon-512.png" /> */}
+        {/* <link rel="apple-touch-icon" href="/img/icon-512.png" /> */}
+        {/* <link rel="manifest" href="/manifest.json" /> */}
+        {/* <meta name="theme-color" content="#06092B" /> */}
+        <meta
+          name="description"
+          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+        />
+      </Head>
     <ThemeProvider theme={theme}>
       <Component {...pageProps} />
       <GlobalStyle />
     </ThemeProvider>
+    </>
   )
 }
 
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,14 +1,23 @@
 import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
+@font-face {
+  font-family: Poppins;
+  font-style: normal;
+  font-weight: 400;
+  src: url(../assets/fonts/Poppins.woff) format('woff'),
+  url(../assets/fonts/Poppins.woff2) format('woff2');
+}
+
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
+  
   body {
     background: ${props => props.theme.colors.background};
     color: ${props => props.theme.colors.text};
-    font: 400 16px Inter, sans-serif;
+    font-family: Poppins, sans-serif;
   }
 `
